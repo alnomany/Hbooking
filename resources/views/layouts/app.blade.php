2231981474f3b12f4fc6f7929ad4abe5642e53e6
alnomany <32714198+alnomany@users.noreply.github.com>
2022-04-20 14:01:02 +0300

    modify

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 	<title>Hotel Login</title>
-	<link rel="shortcut icon" type="image/x-icon" href="{{URL::to('assets/img/favicon.png')}}">
+	<link rel="shortcut icon" type="image/x-icon" href="{{URL::to('assets/img/favicon.pn')}}">
 	<link rel="stylesheet" href="{{URL::to('assets/css/bootstrap.min.css')}}">
 	<link rel="stylesheet" href="{{URL::to('assets/plugins/fontawesome/css/fontawesome.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{URL::to('assets/plugins/fontawesome/css/all.min.css')}}">
